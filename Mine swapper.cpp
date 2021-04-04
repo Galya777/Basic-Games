e9bc01945f2a0575e1910301fdfdadbc538ad702
Galya <58034004+Galya777@users.noreply.github.com>
2021-04-05 01:07:45 +0300

    Mine sweeper with graphics

--- Mine swapper.cpp
+++ Mine swapper.cpp
@@ -1,173 +1,80 @@
 ﻿#include <iostream>
+#include <SFML/Graphics.hpp>
 #include <time.h>
-constexpr auto CELFREE = '$';;
-constexpr auto BOMB = '#';;
-constexpr auto UP =  'w';;
-constexpr auto DOWN = 'x';
-constexpr auto LEFT =  'l';;
-constexpr auto RIGHT = 'r';;
-constexpr auto POSITION = '@';;
-
 using namespace std;
+using namespace sf;
 
-bool finished = false;
-const int MAX = 1000;
- char board[MAX][MAX], userboard[MAX][MAX];
- int userline = 0;
- int usercol = 0;
- bool isinside(int row, int col, int Lines, int Colons) {
-     return 0 <= row && row < Lines && 0 <= col&& col < Colons;
- }
-     
-void putbombs(int bombs, int Lines, int Colons) {
-    srand(time(0));
-    int p = 0;
-    while (p < bombs) {
-        int row = rand() % Lines;
-        int col = rand() % Colons;
-        if (board[row][col] == CELFREE) {
-            board[row][col] = BOMB;
-            p++;
+const int MAX = 100;
+void process(char board[][MAX], int Lines) {
+    for (int i = 1; i <= 10; i++)
+        for (int j = 1; j <= 10; j++)
+        {
+            int n = 0;
+            if (board[i][j] == 9) continue;
+            if (board[i + 1][j] == 9) n++;
+            if (board[i][j + 1] == 9) n++;
+            if (board[i - 1][j] == 9) n++;
+            if (board[i][j - 1] == 9) n++;
+            if (board[i + 1][j + 1] == 9) n++;
+            if (board[i - 1][j - 1] == 9) n++;
+            if (board[i - 1][j + 1] == 9) n++;
+            if (board[i + 1][j - 1] == 9) n++;
+            board[i][j] = n;
         }
-    }
-} bool finish(int bombs, int Lines, int Colons) {
-    int used = Lines*Colons;
-    for (int i = 0; i < Lines; ++i) {
-        for (int j = 0; j < Colons; ++j) {
-            if (userboard[i][j] != CELFREE && userboard[i][j] != POSITION) {
-                used--;
-            }
-        }
-    }if (used == 0) {
-            cout << "Congradulations!" << endl;
-            cout << "You won! " << endl;
-            return true;
-        }
-    return false;
-}
-int countbombs(int row, int col, int Lines, int Colons) {
-    int num = 0;
-    for (int offr = -1; offr <= 1; ++offr) {
-        for (int offc = -1; offc <= 1; ++offc) {
-            int nextline = row + offr, nextcol = col + offc;
-            if (isinside(nextline, nextcol,Lines, Colons) && board[nextline][nextcol] == BOMB) {
-                ++num;
-            }
-        }
-
-
-    }
-    return num;
-}
-void draw(int bombs, int Lines, int Colons) {
-    for (int i = 0; i < Lines; ++i) {
-        for (int j = 0; j < Colons; ++j) {
-            board[i][j] = CELFREE;
-            userboard[i][j] = CELFREE;
-        }
-    }
-    putbombs(bombs, Lines, Colons);
-    for (int i = 0; i < Lines; ++i) {
-        for (int j = 0; j < Colons; ++j) {
-            if (board[i][j] != BOMB) {
-                board[i][j] = countbombs(i, j,Lines,Colons) + '0';
-            }
-        }
-    }
-}
-void print(int Lines, int Colons) {
- system("cls");
-    for (int i = 0; i < Lines; ++i) {
-        for (int j = 0; j < Colons; ++j) {
-            cout << userboard[i][j];
-        }
-        cout << endl;
-    }
-}
-void trigger(int row, int col, int Lines, int Colons) {
-    if (userboard[row][col] != CELFREE && userboard[row][col] != POSITION) {
-        return;
-    }
-    if (board[row][col] == BOMB) {
-        finished = true;
-        cout << "Game over!" <<endl;
-        userboard[row][col] = BOMB;
-        for (int i = 0; i < Lines; ++i) {
-            for (int j = 0; j < Colons; ++j) {
-                if (board[i][j] == BOMB) {
-                    cout << BOMB;
-                }
-                else {
-                    cout << userboard[i][j];
-                }
-            }
-            cout << endl;
-        } 
-    }  else {
-        userboard[row][col] = board[row][col];
-        if (userboard[row][col] == '0') {
-            for (int offr = -1; offr <= 1; ++offr) {
-                for (int offc = -1; offc <= 1; ++offc) {
-                    int nextline = row + offr, nextcol = col + offc;
-                    if (isinside(nextline, nextcol,Lines, Colons)) {
-                        trigger(nextline, nextcol,Lines, Colons);
-                    }
-                }
-            }
-        }
-    }
-}
-void process(char command, int Lines, int Colons) {
-    int next_l = userline, next_co = usercol;
-    if (command == UP) {
-        --next_l;
-    } else if (command == DOWN) {
-        ++next_l;
-    }
-    else if (command == LEFT) {
-        --next_co;
-    }
-    else if (command == RIGHT) {
-        ++next_co;
-    }
-    if (!isinside(next_l, next_co, Lines, Colons)) {
-        return;
-    }
-    userboard[userline][usercol] = board[userline][usercol];
-        userline = next_l;
-        usercol = next_co;
-        userboard[userline][usercol] = POSITION;
-
-            trigger(userline, usercol, Lines, Colons);
-        
-    
-}
-void start(int bombs, int Lines, int Colons) {
-    userline = usercol = 0;
-    finished = false;
-    draw(bombs, Lines, Colons);
-    userboard[userline][usercol] = POSITION;
 }
 int main()
 {
-    int Lines, Colons,bombs;
+    srand(time(0));
+    RenderWindow app(VideoMode(400, 400), "Mine Sweeper!");
+
+    char board[MAX][MAX], userboard[MAX][MAX];
+    int Lines, Colons, bombs;
+    int w = 32;
+    Texture t2;
+    t2.loadFromFile("image/tillllles.jpg");
+    Sprite things(t2);
     cout << "Enter the size of your board!";
     cin >> Lines;
     Colons = Lines;
     cout << "Enter the number of bombs: ";
     cin >> bombs;
-    start(bombs,Lines,Colons);
-    while (!finished) {
-        if (finish(bombs, Lines, Colons) == true) {
-            break;
+    for (int i = 1; i <= Lines; ++i) {
+        for (int j = 1; j <= Colons; ++j) {
+            userboard[i][j] = 10;
+            if (rand() % bombs == 0)  board[i][j] = 9;
+            else board[i][j] = 0;
         }
-        print(Lines, Colons);
-        char command;
-        cout << "Enter a command: " << endl;
-        cin >> command;
-        process(command, Lines, Colons);
-        
+    } process(board, Lines);
+    while (app.isOpen()) {
+        Vector2i pos = Mouse::getPosition(app);
+        int x = pos.x / w;
+        int y = pos.y / w;
+        Event e;
+        while (app.pollEvent(e)) {
+            if (e.type == Event::Closed) {
+                app.close();
+            }
+            if (e.type == Event::MouseButtonPressed) {
+                if (e.key.code == Mouse::Left) {
+                    userboard[x][y] = board[x][y];
+                }
+                else if (e.key.code == Mouse::Right) {
+                    userboard[x][y] = 11;
+                }
+            }
+        }
+        app.clear(Color::Magenta);
+        for (int i = 1; i <= 10; ++i) {
+            for (int j = 1; j <= 10; ++j) {
+                if (userboard[x][y] == 9) userboard[i][j] = board[i][j];
+                things.setTextureRect(IntRect(userboard[i][j] * w, 0, w, w));
+                things.setPosition(i * w, j * w);
+                app.draw(things);
+            }
+        }
+        app.display();
     }
-    return 0;
-}
+        return 0;
+    }
+
 
